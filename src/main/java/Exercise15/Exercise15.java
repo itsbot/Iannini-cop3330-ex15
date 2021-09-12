5bc5deb7d705fdc7ebc2d1b1dbf8290e0cbd2b8f
itsbot <patrickiannini@knights.ucf.edu>
2021-09-12 13:47:47 -0400

    Completed Exercise

--- src/main/java/Exercise15/Exercise15.java
+++ src/main/java/Exercise15/Exercise15.java
@@ -3,9 +3,20 @@
  *  Copyright 2021 Patrick Iannini
  */
 package Exercise15;
+import java.util.Scanner;
 
 public class Exercise15 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a username: ");
+        String username = sc.next();
+        System.out.print("Enter a password: ");
+        String password = sc.next();
+
+        if (username.equals("user1")&& password.equals("Abc123!")) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("I don't know you.");
+        }
     }
 }
